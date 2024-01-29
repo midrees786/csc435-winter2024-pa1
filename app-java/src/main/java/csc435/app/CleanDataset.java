bcd1592f98c4be2e7938e45c69e40794837bba0d
midrees786 <157443030+midrees786@users.noreply.github.com>
2024-01-28 20:40:08 -0600

    Update CleanDataset.java

--- app-java/src/main/java/csc435/app/CleanDataset.java
+++ app-java/src/main/java/csc435/app/CleanDataset.java
@@ -1,27 +1,87 @@
 package csc435.app;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CleanDataset
 {
-    public long dataset_size = 0;
-    public double execution_time = 0.0;
+    private static long datasetSize = 0;
+    private double executionTime = 0.0;
 
-    public void clean_dataset(String input_dir, String output_dir)
-    {
-        // TO-DO implement clean dataset logic
+    public CleanDataset(Path inputdir, Path outdir) throws IOException {
+        long startTime = System.currentTimeMillis();
+        getDirectories(inputdir, inputdir, outdir);
+        long endTime = System.currentTimeMillis();
+
+        executionTime = (endTime - startTime);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         if (args.length != 2) {
             System.err.println("improper number of arguments");
             System.exit(1);
         }
 
-        CleanDataset cleanDataset = new CleanDataset();
+        Path inputdir = Paths.get(args[0]);
+        Path outdir = Paths.get(args[1]);
 
-        cleanDataset.clean_dataset(args[0], args[1]);
-        
-        System.out.print("Finished cleaning " + cleanDataset.dataset_size + " MiB of data");
-        System.out.println(" in " + cleanDataset.execution_time + " miliseconds");
+        try {
+            CleanDataset cleaner = new CleanDataset(inputdir, outdir);
+            cleaner.printResults();
+        } catch (IOException e) {
+            System.err.println("Error cleaning dataset: " + e.getMessage());
+            System.exit(1);
+        }
     }
+
+
+
+    private static void getDirectories(Path currentPath, Path inputdir, Path outputdir) throws IOException {
+        File[] files = currentPath.toFile().listFiles();
+        if(files!=null){
+            for(File file: files){
+                if(file.isDirectory()){
+                    getDirectories(file.toPath(), inputdir, outputdir);;
+                }
+                else if(file.isFile() && file.getName().endsWith(".txt")){
+                    readFile(file.toPath(), inputdir, outputdir);
+                }
+            }
+        }
+    }
+
+    private static void readFile(Path file, Path inputdir, Path outdir ) throws IOException {
+        String content= new String(Files.readAllBytes(file));
+        String cleanedData = cleanData(content);
+
+        Path relative = inputdir.relativize(file);
+        Path output = outdir.resolve(relative);
+        String outPutCleanedPath = output.toString().replace("Dataset", "CleanDataset");
+        output = Paths.get(outPutCleanedPath);
+       // Create File Directories
+        Files.createDirectories(output.getParent());
+        Files.write(output,cleanedData.getBytes());
+
+        datasetSize += file.toFile().length();
+    }
+
+    public void printResults() {
+        System.out.println("Finished cleaning " + datasetSize / (1024.0 * 1024.0) + " MiB of data"
+                + " in " + executionTime + " millisec");
+    }
+    public static String cleanData(String content){
+        content = content.replace("\r","");
+        content = content.replaceAll("\\n{2,}","\n");
+        content = content.replaceAll("[();*\\[\\]:,./#-]", "");
+        return content;
+    }
+
+}
+
+
+
+    
 }
